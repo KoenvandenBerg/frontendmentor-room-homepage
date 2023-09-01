524ec82d84d1ffc126e03fb3155fb9d6298c996d
Koen van den Berg <git@koenvandenberg.dev>
2023-09-01 20:39:46 +0200

    Adjust 'Shop now' hover transition

--- src/components/Homepage/TextCarousel.tsx
+++ src/components/Homepage/TextCarousel.tsx
@@ -15,15 +15,16 @@ export default function TextCarousel(props: TextCarouselProps) {
         <p className="mb-6 text-light-gray leading-[1.375rem]">
           {props.sections[props.currentSection].paragraph}
         </p>
-        <button className="flex items-center hover:text-light-gray hover:path-light-gray transition-colors duration-300">
+        <button className="group flex gap-4 items-center hover:text-light-gray hover:path-light-gray transition-colors duration-300">
           <p className="text-[0.9375rem] tracking-[0.75rem] uppercase">
             Shop now
           </p>
-          <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg">
+          <svg width="50" height="12" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z"
               fill="#000"
               fillRule="nonzero"
+              className="group-hover:fill-light-gray transition-all duration-300 group-hover:translate-x-1"
             />
           </svg>
         </button>
